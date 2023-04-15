1ab13bc8e5f929974319ca7535855312a0bfa884
nikhil-rawat02 <117645790+nikhil-rawat02@users.noreply.github.com>
2023-04-15 23:29:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 function getFormvalue() {
     //Write your code here
+	let first = document.getElementById("fname");
+	let last = document.getElementById("lname");
+	let name = first.value  +" " + last.value;
+	// console.log(first);
+	alert(name);
 
 }
